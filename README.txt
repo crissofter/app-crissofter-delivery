a2c9059bcc3230518476284dac786929e50d48fd
Cristiano Silva <161599431+crissofter@users.noreply.github.com>
2025-11-21 15:30:57 -0300

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,1 @@
+Crissofter Delivery - PWA demo package. Upload index.html and manifest.json to GitHub Pages root.
---
+++ sw.js
@@ -0,0 +1,11 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open('cris-cache').then(function(cache) {
+    return cache.addAll(['./index.html']);
+  }));
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    return response || fetch(event.request);
+  }));
+});
